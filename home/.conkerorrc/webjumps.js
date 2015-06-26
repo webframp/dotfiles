d1d53766cb414463469f09e8c8172a137669860f
Sean Escriva <sean.escriva@gmail.com>
2015-06-25 19:55:37 -0700

    add conkeror webjumps

---
+++ home/.conkerorrc/webjumps.js
@@ -0,0 +1,4 @@
+// webjumps
+define_webjump("em"   , "http://reddit.com/r/emacs");
+define_webjump("gh"  , "http://github.com/%s");
+define_webjump("mw"   , "http://www.merriam-webster.com/dictionary/%s");
