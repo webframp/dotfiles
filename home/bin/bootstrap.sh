8f02efd7b32129cbb002358dbacd9240c8ee14f6
Sean Escriva <sean.escriva@gmail.com>
2017-10-25 20:09:20 -0400

    More running notes on setup stuff

--- home/bin/bootstrap.sh
+++ home/bin/bootstrap.sh
@@ -21,23 +21,31 @@ popd
 popd
 
 # shell setup
-pacman -S zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# relink settings
-need_cmd homeshick
-homeshick link dotfiles --force
+pacman -S zsh exa
 
 # extra packages
 need_cmd yaourt
-yaourt -S haveged clipit xsel xclip rxvt-unicode direnv exa ncurses5-compat-libs lastpass-cli keybase_bin openssh chromium dmenu --noconfirm --needed
-
-# fonts
-yaourt -S ttf-iosevka ttf-ms-fonts --noconfirm --needed
+yaourt --noconfirm --needed -S haveged clipit xsel xclip rxvt-unicode direnv exa ncurses5-compat-libs lastpass-cli keybase_bin openssh chromium pidgin pidgin-sipe dmenu yegonesh homeshick-git
 
 systemctl start haveged
 systemctl enable haveged
 
+# fonts
+yaourt --noconfirm --needed -S ttf-iosevka ttf-ms-fonts
+
+# PulseAudio and video tools
+yaourt --noconfirm --needed -S pulseaudio pulseaudio-alsa pulseaudio-gconf paprefs pamixer pavucontrol
+yaourt --noconfirm --needed -S guvcview
+
 # ArchHaskell and packages
+
+# User specific setup
+useradd -m -g users -G wheel sme
+sudo -u sme sh -c "cd $HOME && sh-c $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# relink settings
+need_cmd homeshick
+sudo -u sme sh -c 'homeshick clone webframp/dotfiles'
+sudo -u sme sh -c 'homeshick link dotfiles --force'
 
 need_cmd() {
 	if ! command -v "$1" > /dev/null 2>&1; then
