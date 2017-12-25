5a3ae3e21c44d42c3242880669b395763cc8ced5
Sean Escriva <sean.escriva@gmail.com>
2017-12-25 01:01:35 -0500

    taffybar color adjustments

--- home/.config/taffybar/taffybar.hs
+++ home/.config/taffybar/taffybar.hs
@@ -20,7 +20,8 @@ import System.Information.Memory
 import System.Information.CPU
 
 memCallback :: IO [Double]
-memCallback = do
+memCallba
+ck = do
   mi <- parseMeminfo
   return [memoryUsedRatio mi]
 
@@ -29,10 +30,32 @@ cpuCallback = do
   (userLoad, systemLoad, totalLoad) <- cpuLoad
   return [totalLoad, systemLoad]
 
+-- see xterm(1) colors
+-- monokai :: [([Char], [Char])]
+-- monokai = [ ("black", "#272822")     -- 0
+--           , ("red", "#f92672")       -- 1
+--           , ("green", "#a6e22e")     -- 2
+--           , ("yellow", "#f4bf75")    -- 3
+--           , ("blue", "#66d9ef")      -- 4
+--           , ("magenta", "#ae81ff")   -- 5
+--           , ("cyan", "#a1efe4")      -- 6
+--           , ("lightgrey", "#f8f8f2") -- 7
+--           -- 8-15
+--           , ("grey", "#75715e")         -- 8
+--           , ("brightred","#f92672")     -- 9
+--           , ("brightgreen", "#a6e22e")  -- 10
+--           , ("brightyellow","#f4bf75")  -- 11
+--           , ("brightblue","#66d9ef")    -- 12
+--           , ("brightmagenta","#ae81ff") -- 13
+--           , ("brightcyan","#a1efe4")    -- 14
+--           , ("white","#f9f8f5")         -- 15
+--           ]
+
 main :: IO()
 main = do
   -- The color components are floating point numbers in the range 0 to 1. If the
   -- values passed in are outside that range, they will be clamped.
+  -- TODO: adjust graph colors
   let memCfg = defaultGraphConfig { graphDataColors = [(0.7098039215686275,0.5372549019607843,0,1)]
                                   , graphLabel = Just "mem"
                                   }
@@ -43,15 +66,16 @@ main = do
                                   }
       pagCfg = defaultPagerConfig { activeWindow     = escape . shorten 40
                                   , activeLayout     = escape
-                                  , activeWorkspace  = colorize "#b58900" "" . wrap "[" "]" . escape
+                                  , activeWorkspace  = colorize "#f4bf75" "" . wrap "[" "]" . escape
                                   , hiddenWorkspace  = escape
                                   , emptyWorkspace   = escape
                                   , visibleWorkspace = wrap "(" ")" . escape
-                                  , urgentWorkspace  = colorize "#dc322f" "#b58900" . escape
+                                  -- TODO change to monokai
+                                  , urgentWorkspace  = colorize "#dc322f" "#b58900" . wrap "<" ">" . escape
                                   , widgetSep        = " : "
                                   }
 
-  let clock = textClockNew Nothing "<span fgcolor='#cb4b16'>%a %b %_d %H:%M</span>" 1
+  let clock = textClockNew Nothing "<span fgcolor='#f92672'>%a %b %_d %H:%M</span>" 1
       pager = taffyPagerNew pagCfg
       note = notifyAreaNew defaultNotificationConfig
       -- wea = weatherNew (defaultWeatherConfig "KSWF") 10 -- KPDX
