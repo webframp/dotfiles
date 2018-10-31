44ee25d71c6c83cc581d2fd56c8f0ff9bcc7c0ca
Sean Escriva <sean.escriva@gmail.com>
2018-10-30 23:02:19 -0400

    better handling for repo fish function

--- home/.config/fish/functions/repo.fish
+++ home/.config/fish/functions/repo.fish
@@ -1,9 +1,18 @@
-# Defined in /var/folders/66/180ynjt55z74l338gh0stq0c0000gn/T//fish.vEMNsi/repo.fish @ line 2
+# Defined in /var/folders/66/180ynjt55z74l338gh0stq0c0000gn/T//fish.dxR987/repo.fish @ line 2
 function repo
-    set --local isitgit (git remote -v | grep 'git@' -c);
-    if test $isitgit -ne 0
-        set --local repo (git remote -v|grep -Eo 'git\@[^ ]+' -m1| sed -e 's/:/\//' | sed -e 's/git\@/https:\/\//'| sed -e 's/\.git//');
-        echo "Opening $repo";
-        command open $repo;
+	set --local sshremote (git remote -v | grep 'git@' -c);
+  set --local httpremote (git remote -v | grep 'http' -c);
+    if test $sshremote -ne 0
+        set repo_url (git remote -v|grep -Eo 'git\@[^ ]+' -m1| sed -e 's/:/\//' | sed -e 's/git\@/https:\/\//'| sed -e 's/\.git//');
+    else if test $httpremote -ne 0
+        set repo_url (git remote -v|grep -Eo 'http\.*[^ ]+' -m1);
+    else
+        set repo_url "none"
+    end
+    if test $repo_url != "none"
+        echo "Opening $repo_url";
+        command open $repo_url;
+    else
+        echo "No repo url found";
     end
 end
