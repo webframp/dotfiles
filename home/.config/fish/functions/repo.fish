2e647f46393ceb864dbc89fb2c62608dba12c8cb
Sean Escriva <sean.escriva@gmail.com>
2019-03-08 17:40:32 -0500

    Keep track of fish helper cmd: repo

--- home/.config/fish/functions/repo.fish
+++ home/.config/fish/functions/repo.fish
@@ -1,6 +1,6 @@
-# Defined in /var/folders/66/180ynjt55z74l338gh0stq0c0000gn/T//fish.dxR987/repo.fish @ line 2
+# Defined in /var/folders/66/180ynjt55z74l338gh0stq0c0000gn/T//fish.0eDUoI/repo.fish @ line 2
 function repo
-	set --local sshremote (git remote -v | grep 'git@' -c);
+	set --local sshremote (git remote -v | grep -e 'git@' -e 'git:' -c);
   set --local httpremote (git remote -v | grep 'http' -c);
     if test $sshremote -ne 0
         set repo_url (git remote -v|grep -Eo 'git\@[^ ]+' -m1| sed -e 's/:/\//' | sed -e 's/git\@/https:\/\//'| sed -e 's/\.git//');
