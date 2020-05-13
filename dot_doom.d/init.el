2c412aab26db2e5b4bb73664377fe3780bfa0cf0
Sean Escriva <sean.escriva@gmail.com>
2020-05-12 22:44:03 -0400

    [emacs] Update latest doom config

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -14,15 +14,15 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
-       ;;ido               ; the other *other* search engine...
+       ;;helm            ; the *other* search engine for love and life
+       ;;ido             ; the other *other* search engine...
        ivy               ; a search engine for love and life
 
        :ui
        ;;deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
-       ;;doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       ;; doom-quit      ; DOOM quit-message prompts when you quit Emacs
        fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;indent-guides     ; highlighted indent columns
