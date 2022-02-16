ed82a0ed0e67bc0e4df229960f8db66f0da5380d
Sean Escriva <sean.escriva@gmail.com>
2022-02-15 20:54:06 -0500

    Refactor local plugins and funcs
    Move some local plugins to lazy autoload behavior
    
    Update plugin list and personal docs links.
    
    General process is:
    
    Use zsh plugins via antibody from personal org forks.  Magit links in
    the org doc allow quick links to review incoming changes in a magit
    buffer before pulling down updates and pushing to a local fork

---
+++ dot_zsh_plugins.sh
@@ -0,0 +1,13 @@
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-zsh-async )
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-zsh-autosuggestions )
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-zsh-completions )
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-base16-shell/base16-shell.plugin.zsh
+fpath+=( /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-base16-shell )
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-powerlevel10k )
+source /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/sme/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-webframp-SLASH-fast-syntax-highlighting )
