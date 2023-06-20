954a2f8eae5aae3fec085160291d7cfaed9b7ed0
Sean Escriva <sean.escriva@gmail.com>
2023-06-20 11:54:45 -0400

    Add starter nix zsh config + fzf,zoxide,direnv,exa
    This primarily uses home-manager to manage zsh config. Zsh is
    configured in global home-manager user config as it's assumed the zsh
    config should be consistent across all environments
    
    A few extra very common tools are installed since a lot of zsh aliases
    depend on these

--- home/sme/global/default.nix
+++ home/sme/global/default.nix
@@ -16,4 +16,38 @@
     homeDirectory = lib.mkDefault "/home/${config.home.username}";
     stateVersion = lib.mkDefault "23.05";
   };
+
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    autocd = true;
+    history = {
+      expireDuplicatesFirst = true;
+      extended = true;
+      ignoreDups = true;
+      ignoreSpace = true;
+    };
+    # https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.plugins
+    # plugins = []
+    # or maybe: https://nix-community.github.io/home-manager/options.html#opt-programs.zsh.zplug.enable
+  };
+
+  programs.fzf = { enable = true; };
+
+  programs.zoxide = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  programs.direnv = {
+    enable = true;
+    enableBashIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  programs.exa = { enable = true; };
+
+  home.file.".gemrc".text = "gem: --no-ri --no-rdoc";
 }
