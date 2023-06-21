7e770809072590beee637147504336eba94db989
Sean Escriva <sean.escriva@gmail.com>
2023-06-21 15:46:04 -0400

    env: move sessionVariables to environment

--- home/sme/global/default.nix
+++ home/sme/global/default.nix
@@ -81,20 +81,6 @@
   home.file.".gemrc".text = "gem: --no-ri --no-rdoc";
   home.file.".p10k.zsh".source = ./includes/p10k.zsh;
 
-  home.sessionVariables = {
-    TERM = "xterm-24bit";
-    LESSCHARSET = "utf-8";
-    # Colors for manpages
-    LESS_TERMCAP_mb = "$'E[01;31m'";
-    LESS_TERMCAP_md = "$'E[01;31m'";
-    LESS_TERMCAP_me = "$'E[0m'";
-    LESS_TERMCAP_se = "$'E[0m'";
-    LESS_TERMCAP_so = "$'E[01;44;33m'";
-    LESS_TERMCAP_ue = "$'E[0m'";
-    LESS_TERMCAP_us = "$'E[01;32m'";
-    BAT_THEME = "base16-256";
-    ZSH_AUTOSUGGEST_USE_ASYNC = 1;
-  };
   # Ensure UTF-8
   home.language = {
     base = "en_US.UTF-8";
