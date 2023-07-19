88d8ba12f7c84e5aceb749cea4b6955465d59fbf
Sean Escriva <sean.escriva@gmail.com>
2023-07-19 16:01:59 -0400

    Move keychain config to single system
    It's not global right now

--- home/sme/galvatron-wsl.nix
+++ home/sme/galvatron-wsl.nix
@@ -1,1 +1,11 @@
-{ inputs, ... }: { imports = [ ./global ]; }
+{ inputs, ... }: {
+  imports = [ ./global ];
+
+  # TODO these files need to exist but it's manual for now
+  programs.keychain = {
+    enable = true;
+    enableZshIntegration = true;
+    agents = [ "ssh" "gpg" ];
+    keys = [ "id_ed25519" "BE06ADB38C7F719D" ];
+  };
+}
