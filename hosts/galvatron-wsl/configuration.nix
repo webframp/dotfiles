13c84685c807b463d8ea78d56d9f0fd2740c9205
Sean Escriva <sean.escriva@gmail.com>
2024-04-05 09:28:13 -0400

    Nix related packages are in global
    Remove from machine specific config

--- hosts/galvatron-wsl/configuration.nix
+++ hosts/galvatron-wsl/configuration.nix
@@ -145,13 +145,6 @@
     firefox
     gnome.gnome-tweaks
 
-    # Nix related
-    cachix
-    direnv
-    nix-index
-    nox
-    patchelf
-
     # afterwards run: (these should be automatic somehow)
     # granted browser set -b firefox -p /mnt/c/Users/sme/scoop/shims/firefox.exe (if using non-wsl ff)
     # granted browser set-sso -b firefox
