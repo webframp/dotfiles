af0dd11c809d7152e1904b6cc77add9c99852b5f
Sean Escriva <sean.escriva@gmail.com>
2024-06-26 14:58:25 -0400

    Adding nix-darwin

---
+++ darwin/bluestreak/configuration.nix
@@ -0,0 +1,18 @@
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.home-manager.darwinModules.home-manager
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.sme = import ../../home/sme/bluestreak.nix;
+  };
+}
