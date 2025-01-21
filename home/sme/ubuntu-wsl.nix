5804b9a808d0e1a8c2daece4c85b45607c77da0f
Sean Escriva <sean.escriva@gmail.com>
2025-01-21 09:09:49 -0500

    ubuntu wsl tweaks

--- home/sme/ubuntu-wsl.nix
+++ home/sme/ubuntu-wsl.nix
@@ -8,4 +8,11 @@
   imports = [./global];
 
   home.shellAliases = {yay = "home-manager switch --flake .#sme@ubuntu";};
+
+  programs.keychain = {
+    enable = true;
+    enableZshIntegration = true;
+    agents = ["ssh"];
+    keys = ["id_ed25519"];
+  };
 }
