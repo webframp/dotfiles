b27ec01fd7140686031858c2191f5159a50490b0
Sean Escriva <sean.escriva@gmail.com>
2025-07-24 16:31:33 +0200

    emacs: NativeComp
    Seems to be fixed now, not sure what the issues with these upstream
    issues.
    
    Removing was a workaround for:
    
    - https://github.com/NixOS/nixpkgs/issues/395169
    
    Related PR:
    
    - https://github.com/NixOS/nixpkgs/pull/398156

--- home/sme/bluestreak.nix
+++ home/sme/bluestreak.nix
@@ -425,7 +425,7 @@ in {
     enable = true;
     # workaround for https://github.com/NixOS/nixpkgs/issues/395169
     # can remove possibly after https://github.com/NixOS/nixpkgs/pull/398156
-    package = pkgs.emacs.override {withNativeCompilation = false;};
+    package = pkgs.emacs.override {withNativeCompilation = true;};
   };
 
   programs.eza.enable = true;
