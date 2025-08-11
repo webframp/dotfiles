342a982ccc2c6686a64689dc63da3d88e071d1d2
Sean Escriva <sean.escriva@gmail.com>
2025-08-11 09:33:40 +0200

    Remove VSCode
    I don't use it, I don't need it here

--- home/sme/bluestreak.nix
+++ home/sme/bluestreak.nix
@@ -540,31 +540,6 @@ in {
   # programs.jqp
   # programs.zsh.initContent
 
-  # Make sure VSCode is available and configured I guess?
-  programs.vscode.profiles.default = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      davidanson.vscode-markdownlint
-      editorconfig.editorconfig
-      esbenp.prettier-vscode
-      continue.continue
-      golang.go
-      hashicorp.hcl
-      hashicorp.terraform
-      ms-azuretools.vscode-docker
-      oderwat.indent-rainbow
-      redhat.vscode-yaml
-      # ms-dotnettools.dotnet-interactive-vscode
-      ms-toolsai.jupyter
-      ms-dotnettools.csharp
-    ];
-    userSettings = {
-      "telemetry.telemetryLevel" = "off";
-      "editor.renderWhitespace" = "all";
-      "editor.defaultFormatter" = "esbenp.prettier-vscode";
-    };
-  };
-
   services.home-manager.autoExpire = {
     enable = true;
     frequency = "weekly";
