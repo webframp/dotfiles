c6f565e1a1a5b444de69f39b80356766bea7b672
Sean Escriva <sean.escriva@gmail.com>
2025-08-18 01:16:55 -0400

    fix vscode-server refs

--- hosts/galvatron-wsl/configuration.nix
+++ hosts/galvatron-wsl/configuration.nix
@@ -25,7 +25,7 @@
     # https://github.com/nix-community/nixos-vscode-server
     (fetchTarball {
       url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-      sha256 = "1l77kybmghws3y834b1agb69vs6h4l746ga5xccvz4p1y8wc67h7";
+      sha256 = "1rdn70jrg5mxmkkrpy2xk8lydmlc707sk0zb35426v1yxxka10by";
     })
   ];
 
@@ -323,7 +323,7 @@
   #  wantedBy = [ "multi-user.target" ];
   #};
 
-  services.vscode-server.enable = true;
+  services.vscode-server.enable = false;
 
   virtualisation = {
     podman = {
