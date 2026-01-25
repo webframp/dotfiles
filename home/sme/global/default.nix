936db8e687ea1f211b8bc48490f5512b31dce269
Sean Escriva <sean.escriva@gmail.com>
2026-01-24 17:56:22 -0800

    Add git module for configuration
    Refactored Git configuration by introducing a new module.
    
    This allows consistent and reusable Git settings, aliases, and signing
    keys across hosts.
    
    The overhaul includes:
    
    - Migration of Git configuration from `bluestreak.nix`
    - Added options for custom Git credential helpers
    - Declared standard Git aliases and preferences in a shareable format.

--- home/sme/global/default.nix
+++ home/sme/global/default.nix
@@ -14,6 +14,7 @@
     delta
     direnv
     fzf
+    git
     tmux
   ];
 
@@ -155,6 +156,7 @@
     whitelist = ["~/src/o11n"];
   };
   custom.fzf.enable = true;
+  custom.git.enable = true;
   custom.tmux = {
     enable = true;
     terminal = "tmux-256color";
