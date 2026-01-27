6f90bc92a7235020e55dbb86dacf6f8ff2d0bf01
Sean Escriva <sean.escriva@gmail.com>
2026-01-27 12:49:03 -0500

    move linux specific settings to separate module
    now imported by each platform using linux

---
+++ home/sme/linux.nix
@@ -0,0 +1,26 @@
+# ABOUTME: Linux-specific home-manager configuration shared across all Linux hosts
+# ABOUTME: Imported by galvatron-wsl, ubuntu-wsl, and generic configurations
+{pkgs, ...}: {
+  gtk.enable = true;
+
+  home.file.".gemrc".text = "gem: --no-ri --no-rdoc";
+
+  home.file.".icons/default".source = "${pkgs.vanilla-dmz}/share/icons/Vanilla-DMZ";
+
+  home.file.".xterm-24bit.terminfo" = {
+    source = ./global/includes/xterm-24bit.terminfo;
+    onChange = "tic -x -o ~/.terminfo ~/.xterm-24bit.terminfo";
+  };
+
+  # Ensure UTF-8
+  home.language = {
+    base = "en_US.UTF-8";
+    ctype = "en_US.UTF-8";
+    numeric = "en_US.UTF-8";
+    time = "en_US.UTF-8";
+    collate = "en_US.UTF-8";
+    monetary = "en_US.UTF-8";
+    messages = "en_US.UTF-8";
+    name = "en_US.UTF-8";
+  };
+}
