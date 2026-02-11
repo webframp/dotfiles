75a09fe61f5cd0a23a6868392dcbd845dad01910
Sean Escriva <sean.escriva@gmail.com>
2026-02-11 15:21:39 -0500

    fix build: go-jwlm

--- pkgs/go-jwlm/default.nix
+++ pkgs/go-jwlm/default.nix
@@ -19,6 +19,9 @@ buildGoModule rec {
 
   vendorHash = "sha256-jC5B3gLM0QA9KqqN0HGx9xlActcjyI3YAzI/M6POVjQ=";
 
+  # Tests require network access (Test_DownloadCatalogRealLife)
+  doCheck = false;
+
   meta = with lib; {
     description = "A command line tool to easily merge JW Library backups, written in Go.";
     homepage = "https://github.com/AndreasSko/go-jwlm";
