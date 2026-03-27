221e9ae1fb67380edfa70fa0966341244ab345c2
Sean Escriva <sean.escriva@gmail.com>
2026-03-26 23:17:47 -0400

    add swamp package

---
+++ pkgs/swamp/default.nix
@@ -0,0 +1,51 @@
+# ABOUTME: Swamp CLI - AI automation platform for developers
+# ABOUTME: Fetches prebuilt binaries from GitHub releases
+{
+  lib,
+  stdenv,
+  fetchurl,
+  makeWrapper,
+  ...
+}:
+let
+  version = "20260327.005915.0-sha.86500cc2";
+
+  sources = {
+    x86_64-linux = {
+      url = "https://github.com/systeminit/swamp/releases/download/v${version}/swamp-linux-x86_64";
+      hash = "sha256-LtuT/41bcz0KTcTavXuYWRSxVuzYHCmptPVGjmA6pQI=";
+    };
+    aarch64-darwin = {
+      url = "https://github.com/systeminit/swamp/releases/download/v${version}/swamp-darwin-aarch64";
+      hash = "sha256-2H4YA6v8pcXhem8H7xSFGInRpBRiZINF/MCfjbRVpNE=";
+    };
+  };
+
+  src = fetchurl sources.${stdenv.system};
+in
+stdenv.mkDerivation {
+  pname = "swamp";
+  inherit version src;
+
+  dontUnpack = true;
+  dontStrip = true;
+  dontPatchELF = true;
+
+  nativeBuildInputs = [makeWrapper];
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm755 $src $out/libexec/swamp
+    makeWrapper $out/libexec/swamp $out/bin/swamp \
+      ${lib.optionalString stdenv.isLinux "--prefix LD_LIBRARY_PATH : ${stdenv.cc.cc.lib}/lib"}
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "AI automation platform for developers with human review controls";
+    homepage = "https://swamp.club";
+    license = licenses.asl20;
+    platforms = builtins.attrNames sources;
+    mainProgram = "swamp";
+  };
+}
