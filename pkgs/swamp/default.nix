c7d9caee968bb78b36ba6016630f866ed9158c7c
Sean Escriva <sean.escriva@gmail.com>
2026-07-07 06:01:31 -0400

    swamp update

--- pkgs/swamp/default.nix
+++ pkgs/swamp/default.nix
@@ -8,16 +8,16 @@
   ...
 }:
 let
-  version = "20260705.012006.0-sha.9055d66d";
+  version = "20260707.035329.0-sha.84dbdd93";
 
   sources = {
     x86_64-linux = {
       url = "https://github.com/swamp-club/swamp/releases/download/v${version}/swamp-linux-x86_64";
-      hash = "sha256-mVmMm3451arzIWpCrhOtMzhpNo1dzJFSGth5lB/sa0o=";
+      hash = "sha256-QX4gpxA8fdwQ1shEE1WSBnkk7eu9oHSrnpXi3AHd3sM=";
     };
     aarch64-darwin = {
       url = "https://github.com/swamp-club/swamp/releases/download/v${version}/swamp-darwin-aarch64";
-      hash = "sha256-0i9Qkq0jbr6fEgOUgnx2WLwDmW2a713b4/r4gXjykv8=";
+      hash = "sha256-J/m0PgI9uZfnKmv3xb1puLQxiK7/bqEDLdY3P3iNwAg=";
     };
   };
 
