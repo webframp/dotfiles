06f868b899a3062a128f20d1d0fdcefb3473c110
Sean Escriva <sean.escriva@gmail.com>
2026-09-08 12:02:07 -0700

    swamp update

--- pkgs/swamp/default.nix
+++ pkgs/swamp/default.nix
@@ -8,16 +8,16 @@
   ...
 }:
 let
-  version = "20260902.180719.0-sha.949b0979";
+  version = "20260908.175912.0-sha.14d401ea";
 
   sources = {
     x86_64-linux = {
       url = "https://artifacts.swamp-club.com/swamp/${version}/binary/linux/x86_64/swamp-${version}-binary-linux-x86_64.tar.gz";
-      hash = "sha256-JMWsnxTfoZ5c1th7YZO2kG636RObEXAwSpZaW7gJbqc=";
+      hash = "sha256-H7u/XuTb/nee93741WOdLi7KM70s5eesp1aTZy+GpBg=";
     };
     aarch64-darwin = {
       url = "https://artifacts.swamp-club.com/swamp/${version}/binary/darwin/aarch64/swamp-${version}-binary-darwin-aarch64.tar.gz";
-      hash = "sha256-aQBJyiPQvx4piUvd7gr+xiWPIJLqhBB/CX4KI2J6Z2U=";
+      hash = "sha256-25pioZ0m3/5g7idRgYFGCCwoo0jRgRw+8jF7rp84D8A=";
     };
   };
 
